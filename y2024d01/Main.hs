5d9d52f66f3f800b67605b9d6ad4291d304d42bb
Dan Vanderkam <danvdk@gmail.com>
2025-11-24 16:59:46 -0500

    this is an interesting error

--- y2024d01/Main.hs
+++ y2024d01/Main.hs
@@ -8,9 +8,11 @@ main = do
     let inputFile = head args
     content <- readFile inputFile
     let numbers = map words (lines content)
-    let list1 = fmap (read . head) numbers
-    let list2 = fmap (read . (!!1)) numbers
-    let answer1 = sum $ zipWith (\a b -> abs $ subtract a b) (sort list1) (sort list2)
+    let list1 = map (read . head) numbers
+    let list2 = map (read . (!!1)) numbers
+    let pairs = zip (sort list1) (sort list2)
+    let deltas = map (\a b -> abs (a - b)) pairs
+    let answer1 = sum deltas
     print answer1
     let counts = Map.fromListWith (+) (zip list2 (repeat 1))
     let answer2 = sum $ fmap (\x -> x * (fromMaybe 0 (Map.lookup x counts))) list1
