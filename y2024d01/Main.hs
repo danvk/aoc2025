8cdd564f73916a048153ecb1b2132f59eb0b7dd5
Dan Vanderkam <danvdk@gmail.com>
2025-11-24 17:03:59 -0500

    apply suggestions

--- y2024d01/Main.hs
+++ y2024d01/Main.hs
@@ -11,9 +11,8 @@ main = do
     let list1 = map (read . head) numbers
     let list2 = map (read . (!!1)) numbers
     let pairs = zip (sort list1) (sort list2)
-    let deltas = map (\(a, b) -> abs (a - b)) pairs
-    let answer1 = sum deltas
+    let answer1 = sum $ map (\(a, b) -> abs (a - b)) pairs
     print answer1
-    let counts = Map.fromListWith (+) (zip list2 (repeat 1))
-    let answer2 = sum $ fmap (\x -> x * (fromMaybe 0 (Map.lookup x counts))) list1
+    let counts = Map.fromListWith (+) (map (, 1) list2)
+    let answer2 = sum $ fmap (\x -> x * fromMaybe 0 (Map.lookup x counts)) list1
     print answer2
