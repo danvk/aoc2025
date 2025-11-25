dd555c6715d782dccde1f7f12e6a8097c5106a08
Dan Vanderkam <danvdk@gmail.com>
2025-11-25 14:12:43 -0500

    y2015d03 star 2

--- y2015d03/Main.hs
+++ y2015d03/Main.hs
@@ -1,6 +1,8 @@
 -- https://adventofcode.com/2015/day/3
 import System.Environment (getArgs)
 import qualified Data.Map.Strict as Map
+import Data.List.Split
+import Data.List
 
 toDir :: Char -> (Int, Int)
 toDir '<' = (-1, 0)
@@ -13,6 +15,15 @@ toDir c = error ("Invalid character '" ++ [c] ++ "'")
 move :: (Int, Int) -> (Int, Int) -> (Int, Int)
 move (a, b) (c, d) = (a+c, b+d)
 
+
+toPair :: Show a => [a] -> (a, a)
+toPair [x, y] = (x, y)
+toPair x = error ("was not pair " ++ (show x))
+
+unzipList :: Show a => [a] -> ([a], [a])
+unzipList xs = toPair $ transpose $ chunksOf 2 xs
+
+
 main :: IO ()
 main = do
     args <- getArgs
@@ -23,3 +34,9 @@ main = do
     let counts = Map.fromListWith (+) (map (, 1::Int) coords)
     let part1 = Map.size counts
     print part1
+    let (santaDirs, roboSantaDirs) = unzipList dirs
+    let santaCoords = scanl move (0, 0) santaDirs
+    let roboSantaCoords = scanl move (0, 0) roboSantaDirs
+    let counts2 = Map.fromListWith (+) (map (, 1::Int) (santaCoords ++ roboSantaCoords))
+    let part2 = Map.size counts2
+    print part2
