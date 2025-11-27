9b2ca5a7cca4a8fb90b84eeeebb655c5ea5c3e6a
Dan Vanderkam <danvdk@gmail.com>
2025-11-27 11:11:02 -0500

    have a bug! add sample inputs

--- y2015d06/Main.hs
+++ y2015d06/Main.hs
@@ -1,7 +1,7 @@
 -- https://adventofcode.com/2015/day/6
 import System.Environment (getArgs)
-
 import Data.List.Split
+import Data.Map.Strict as Map hiding (map, foldr)
 
 toPair :: Show a => [a] -> (a, a)
 toPair [a, b] = (a, b)
@@ -25,12 +25,33 @@ parseLine str = case words str of
     ["turn", "off", a, "through", b] -> ("off", parseXY a, parseXY b)
     _ -> error $ "Invalid line: " ++ str
 
+mergeLight :: String -> Bool -> Bool
+mergeLight "on" _ = True
+mergeLight "off" _ = False
+mergeLight "toggle" x = not x
+mergeLight op _ = error $ "Invalid op " ++ op
+
+updateMap :: Ord a => Map a b -> Map a b -> Map a b
+updateMap m1 m2 = Map.union m2 m1
+
+applyOp :: (String, (Int, Int), (Int, Int)) -> Map (Int, Int) Bool -> Map (Int, Int) Bool
+applyOp (op, (x1, y1), (x2, y2)) m =
+    let rect = [(x, y) | x <- [x1..x2], y <- [y1..y2]] in
+    let updates = Map.fromList $ map (\xy -> (xy, mergeLight op (Map.findWithDefault False xy m))) rect
+    in updateMap m updates
+
+countTrues :: Ord a => Map a Bool -> Int
+countTrues m = length [pos | (pos, v) <- Map.toList m, v]
+
 main :: IO ()
 main = do
     args <- getArgs
     let inputFile = head args
     content <- readFile inputFile
     let commands = map parseLine $ lines content
-    print commands
+    let grid = foldr applyOp Map.empty commands
+    let part1 = countTrues grid
+    print part1
+
 
 -- (a->b) -> (b->c) -> (a->c)
