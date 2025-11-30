9ca2aa5c668c08986cef4ea553f759a57d8de02c
Dan Vanderkam <danvdk@gmail.com>
2025-11-30 09:05:44 -0500

    y2015d08 star 2

--- y2015d08/Main.hs
+++ y2015d08/Main.hs
@@ -11,6 +11,11 @@ parse ('\\':'x':a:b:xs) = chr (read ("0x" ++ [a, b]) :: Int) : parse xs
 parse ('"':xs) = parse xs
 parse (x:xs) = x:parse xs
 
+part2 :: Char -> Int
+part2 '"' = 2
+part2 '\\' = 2
+part2 _ = 1
+
 main :: IO ()
 main = do
     args <- getArgs
@@ -23,3 +28,6 @@ main = do
     let diffs = zipWith (\ a b -> length a - length b) raws parsed
         part1 = sum diffs
     print part1
+    let rawLens = sum $ map length raws
+        part2Sum = sum $ map ((+2 ) . sum . map part2) raws
+    print $ part2Sum - rawLens
