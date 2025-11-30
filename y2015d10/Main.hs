abe6df30f21d482c0527d82bfb99b135d63b3f63
Dan Vanderkam <danvdk@gmail.com>
2025-11-30 11:14:45 -0500

    refactor to use group

--- y2015d10/Main.hs
+++ y2015d10/Main.hs
@@ -1,10 +1,9 @@
 -- https://adventofcode.com/2015/day/10
 import System.Environment (getArgs)
+import Data.List
 
 lookAndSay :: [Int] -> [Int]
-lookAndSay [] = []
-lookAndSay (x:xs) = (1 + length start):x:lookAndSay rest
-    where (start, rest) = break (/= x) xs
+lookAndSay xs = group xs >>= \x -> [length x, head x]
 
 main :: IO ()
 main = do
