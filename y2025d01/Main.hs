48b96e0ef367881919fcb08fda9f42eaaac3aa17
Dan Vanderkam <danvdk@gmail.com>
2025-12-01 08:32:37 -0500

    clean up

--- y2025d01/Main.hs
+++ y2025d01/Main.hs
@@ -1,39 +1,26 @@
 -- https://adventofcode.com/2025/day/1
 import System.Environment (getArgs)
 
--- L30
--- R48
-
 parseLine :: String -> Int
-parseLine ('L':xs) = -(read xs)
-parseLine ('R':xs) = read xs
+parseLine ('L' : xs) = -(read xs)
+parseLine ('R' : xs) = read xs
 parseLine line = error $ "Invalid line " ++ line
 
 turn :: Int -> Int -> Int
 turn a b = (a + b) `mod` 100
 
 clicks :: Int -> Int -> Int
-clicks a b
-    | a < b = length $ filter ((==) 0 . (`mod` 100)) [(a+1)..b]
-    | otherwise = length $ filter ((==) 0 . (`mod` 100)) [(a-1),(a-2)..b]
-
--- rotations in [-998, 999]
+clicks a b = length $ filter ((==) 0 . (`mod` 100)) (if a < b then [(a + 1) .. b] else [(a - 1), (a - 2) .. b])
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let inputFile = head args
-    content <- readFile inputFile
-    let rotations = map parseLine $ lines content
-        positions = scanl turn 50 rotations
-        zeros = filter (== 0) positions
-        part1 = length zeros
-    print part1
-    let abspos = scanl (+) 50 rotations
-        -- part2 = sum $ zipWith clicks abspos (tail abspos)
-        part2 = zip abspos (tail abspos)
-        deb = map (uncurry clicks) part2
-    -- print abspos
-    print part2
-    print deb
-    print $ sum deb
+  args <- getArgs
+  let inputFile = head args
+  content <- readFile inputFile
+  let rotations = map parseLine $ lines content
+      positions = scanl turn 50 rotations
+      part1 = length $ filter (== 0) positions
+      abspos = scanl (+) 50 rotations
+      part2 = sum $ zipWith clicks abspos (tail abspos)
+  print part1
+  print part2
