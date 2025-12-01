d61622ed81c8677e82c6f64edf88c1ad4fd57dcd
Dan Vanderkam <danvdk@gmail.com>
2025-12-01 08:27:31 -0500

    y2025d01 star 2

--- y2025d01/Main.hs
+++ y2025d01/Main.hs
@@ -12,6 +12,13 @@ parseLine line = error $ "Invalid line " ++ line
 turn :: Int -> Int -> Int
 turn a b = (a + b) `mod` 100
 
+clicks :: Int -> Int -> Int
+clicks a b
+    | a < b = length $ filter ((==) 0 . (`mod` 100)) [(a+1)..b]
+    | otherwise = length $ filter ((==) 0 . (`mod` 100)) [(a-1),(a-2)..b]
+
+-- rotations in [-998, 999]
+
 main :: IO ()
 main = do
     args <- getArgs
@@ -22,3 +29,11 @@ main = do
         zeros = filter (== 0) positions
         part1 = length zeros
     print part1
+    let abspos = scanl (+) 50 rotations
+        -- part2 = sum $ zipWith clicks abspos (tail abspos)
+        part2 = zip abspos (tail abspos)
+        deb = map (uncurry clicks) part2
+    -- print abspos
+    print part2
+    print deb
+    print $ sum deb
