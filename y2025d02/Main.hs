381e51d134a3ecd8ab90da0bc937792809aeb7fc
Dan Vanderkam <danvdk@gmail.com>
2025-12-02 09:33:02 -0500

    y2025d02 star 2

--- y2025d02/Main.hs
+++ y2025d02/Main.hs
@@ -18,14 +18,22 @@ isInvalidNumber num = left == right
     str = show num
     (left, right) = splitAt (length str `div` 2) str
 
+isInvalid2 :: Int -> Bool
+isInvalid2 num = any testN [1 .. (len `div` 2)]
+  where
+    str = show num
+    len = length str
+    testN n = str == concat (replicate (len `div` n) stem)
+      where
+        stem = take n str
+
 main :: IO ()
 main = do
   args <- getArgs
   let inputFile = head args
   content <- readFile inputFile
   let pairs = parseRanges content
-      counts = map (\(a, b) -> b - a) pairs
       part1 = sum $ pairs >>= (\(a, b) -> filter isInvalidNumber [a .. b])
-  print pairs
-  print counts
+      part2 = sum $ pairs >>= (\(a, b) -> filter isInvalid2 [a .. b])
   print part1
+  print part2
