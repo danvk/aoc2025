7ff2fb1b4d9f35da2daa8f3481dc8ddeeef264cf
Dan Vanderkam <danvdk@gmail.com>
2025-12-03 17:38:39 -0500

    y2015d19 star 1

---
+++ y2015d19/Main.hs
@@ -0,0 +1,49 @@
+-- https://adventofcode.com/2015/day/19
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+
+import Data.Char
+import Data.List
+import Data.List.Split
+import Data.Map qualified as M
+import System.Environment (getArgs)
+
+parseReplacement :: String -> (String, [String])
+parseReplacement str =
+  let [left, right] = splitOn " => " str
+   in (left, parseMolecule right)
+
+parseMolecule :: String -> [String]
+parseMolecule "" = []
+parseMolecule (x : y : xs)
+  | isUpperCase x && isLowerCase y = [x, y] : parseMolecule xs
+parseMolecule (x : xs) = [x] : parseMolecule xs
+
+indexReplacements :: [(String, [String])] -> M.Map String [[String]]
+indexReplacements reps = M.fromListWith (++) $ map (\(lhs, rhs) -> (lhs, [rhs])) reps
+
+applyReplacement :: M.Map String [[String]] -> [String] -> [[String]]
+applyReplacement _ [] = []
+applyReplacement repMap (x : xs) =
+  if x `M.member` repMap
+    then
+      ( let replaceIt = [rep ++ xs | rep <- repMap M.! x]
+         in let dontReplaceIt = map (x :) $ applyReplacement repMap xs
+             in replaceIt ++ dontReplaceIt
+      )
+    -- TODO: dedupe this clause
+    else map (x :) $ applyReplacement repMap xs
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let inputFile = head args
+  content <- readFile inputFile
+  let inLines = lines content
+      splitted = splitOn [""] inLines
+      [replacementStrs, [inputMoleculeStr]] = splitted
+      replacements = indexReplacements $ map parseReplacement replacementStrs
+      inputMolecule = parseMolecule inputMoleculeStr
+      part1 = length $ nub $ applyReplacement replacements inputMolecule
+  print replacements
+  print inputMolecule
+  print part1
