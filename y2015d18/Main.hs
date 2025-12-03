0353b6a593f83b8836f102b35f6149fb0b46ee39
Dan Vanderkam <danvdk@gmail.com>
2025-12-03 16:13:07 -0500

    Size (Int, Int)

--- y2015d18/Main.hs
+++ y2015d18/Main.hs
@@ -6,18 +6,20 @@ import System.Environment (getArgs)
 
 type Point = (Int, Int)
 
+type Size = (Int, Int)
+
 type Grid = M.Map Point Bool
 
-parseGrid :: String -> (Int, Int, Grid)
-parseGrid str = (w, h, grid)
+parseGrid :: String -> (Size, Grid)
+parseGrid str = ((w, h), grid)
   where
     rows = lines str
     h = length rows
     w = length $ head rows
     grid = M.fromList [((x, y), c == '#') | (y, row) <- zip [0 ..] rows, (x, c) <- zip [0 ..] row]
 
-gridToStr :: Int -> Int -> Grid -> String
-gridToStr w h g = intercalate "\n" $ map rowToStr [0 .. (h - 1)]
+gridToStr :: Size -> Grid -> String
+gridToStr (w, h) g = intercalate "\n" $ map rowToStr [0 .. (h - 1)]
   where
     rowToStr y = [if M.findWithDefault False (x, y) g then '#' else '.' | x <- [0 .. (w - 1)]]
 
@@ -26,6 +28,6 @@ main = do
   args <- getArgs
   let inputFile = head args
   content <- readFile inputFile
-  let (w, h, initState) = parseGrid content
-  print (w, h)
-  putStrLn $ gridToStr w h initState
+  let (dims, initState) = parseGrid content
+  print dims
+  putStrLn $ gridToStr dims initState
