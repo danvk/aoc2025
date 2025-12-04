029b1878cb883dccfff6f64c8c6fc566014f5976
Dan Vanderkam <danvdk@gmail.com>
2025-12-04 16:05:51 -0500

    steps_to_target

--- y2015d19/part2.py
+++ y2015d19/part2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import re
+import sys
 
 lines = [x.strip() for x in open('input/2015/day19/input.txt').readlines()]
 molecule = lines[-1]
@@ -9,6 +9,8 @@ print(molecule)
 print(productions)
 
 assert len({rhs for _, rhs in productions}) == len(productions)
+rhs_to_prev = {rhs: lhs for lhs, rhs in productions}
+assert len(rhs_to_prev) == len(productions)
 
 easyforces = [(lhs, rhs) for lhs, rhs in productions if 'Rn' in rhs and 'Ar' in rhs]
 normals = [(lhs, rhs) for lhs, rhs in productions if 'Rn' not in rhs and lhs != 'e']
@@ -31,6 +33,9 @@ for lhs, rhs in normals:
     repMap[lhs].append(parse_molecule(rhs))
 
 
+targets = {''.join(parse_molecule(rhs)[2:-1]) for _, rhs in easyforces}
+print(f'{targets=}')
+
 def apply_replacement(molecule: list[str]):
     if not molecule:
         return []
@@ -51,15 +56,62 @@ def step(reps: list[str, str]) -> list[str, str]:
     return out
 
 
+def find_leaf(molecule: str):
+    """Find the shortest 'leaf' Rn..Ar span."""
+    spans = []
+    last_rn = -1
+    for i in range(len(molecule)):
+        if molecule[i:i+2] == 'Rn':
+            last_rn = i
+        elif molecule[i:i+2] == 'Ar':
+            if last_rn != -1:
+                spans.append((last_rn, i + 2))
+                last_rn = -1
+    return min(spans, key=lambda x: x[1] - x[0])
+
+
+def all_matches(haystack: str, needle: str):
+    indices = []
+    start = 0
+    while True:
+        index = haystack.find(needle, start)
+        if index == -1:
+            break
+        indices.append(index)
+        start = index + 1  # Start searching from the character after the found substring
+    return indices
+
+
+def steps_to_target(molecules: set[str], targets: set[str]):
+    assert molecules
+    if not molecules.isdisjoint(targets):
+        match = [*(molecules & targets)][0]
+        return [match]
+    prevs = {}
+    for molecule in molecules:
+        for rhs, lhs in rhs_to_prev.items():
+            for idx in all_matches(molecule, rhs):
+                prev = molecule[:idx] + lhs + molecule[idx+len(rhs):]
+                prevs[prev] = molecule
+    all_prevs = set(prevs.keys())
+    seq = steps_to_target(all_prevs, targets)
+    return [prevs[seq[0]]] + seq
+
+print(targets)
+print(steps_to_target({'CaCaSi'}, {'Si'}))
+print(steps_to_target({'SiAlYSiAl'}, targets))
+print(steps_to_target({'FYCaPTiBCaSiThCaSiThPMg'}, targets))
+print(steps_to_target({'TiBSiThCaCaSiThCaF'}, targets))
+
 # TODO: need to unique these by shortest path
 shorts = step(normals)
-shorts2 = step(shorts)
-shorts3 = step(shorts2)
-shorts4 = step(shorts3)
-shorts5 = step(shorts4)
+# shorts2 = step(shorts)
+# shorts3 = step(shorts2)
+# shorts4 = step(shorts3)
+# shorts5 = step(shorts4)
 
 
-for lhs, rhs in normals + shorts + shorts2 + shorts3 + shorts4 + shorts5:
+for lhs, rhs in normals + shorts:  # + shorts2 + shorts3 + shorts4 + shorts5:
     easyforces.append((f'Rn{lhs}Ar', f'Rn{rhs}Ar'))
     easyforces.append((f'Y{lhs}Ar', f'Y{rhs}Ar'))
     easyforces.append((f'Rn{lhs}Y', f'Rn{rhs}Y'))
@@ -78,13 +130,13 @@ while True:
             is_match = True
             num += 1
             molecule = molecule.replace(rhs, lhs, 1)
-    start = molecule
-    molecule = re.sub(r'Ti(Ti)+Ti', 'TiTi', molecule)
-    molecule = re.sub(r'Ca(Ca)+Ca', 'CaCa', molecule)
-    if molecule != start:
-        is_match = True
-        num += 1
-        print('  (regex)')
+    # start = molecule
+    # molecule = re.sub(r'Ti(Ti)+Ti', 'TiTi', molecule)
+    # molecule = re.sub(r'Ca(Ca)+Ca', 'CaCa', molecule)
+    # if molecule != start:
+    #     is_match = True
+    #     num += 1
+    #     print('  (regex)')
     if not is_match:
         break
 
