d5962ef9312078534f1b66c8ecd04d6247040265
Dan Vanderkam <danvdk@gmail.com>
2025-12-05 09:12:37 -0500

    y2025d05 star 2

--- y2025d05/Main.hs
+++ y2025d05/Main.hs
@@ -1,16 +1,25 @@
 -- https://adventofcode.com/2025/day/5
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
+import Data.List
 import Data.List.Split
 import System.Environment (getArgs)
 
 parseRange :: String -> (Int, Int)
 parseRange str =
   let [left, right] = map (read @Int) $ splitOn "-" str
-   in (left, right)
+   in (left, right + 1)
 
 inRange :: (Int, Int) -> Int -> Bool
-inRange (a, b) x = x >= a && x <= b
+inRange (a, b) x = x >= a && x < b
+
+inRangeInc :: (Int, Int) -> Int -> Bool
+inRangeInc (a, b) x = x >= a && x <= b
+
+splitRange :: [Int] -> (Int, Int) -> [(Int, Int)]
+splitRange edges r = zip pts (tail pts)
+  where
+    pts = sort $ filter (r `inRangeInc`) edges
 
 main :: IO ()
 main = do
@@ -23,7 +32,14 @@ main = do
       ingredients = map (read @Int) ingredientStrs
       freshIngredients = filter (\x -> any (`inRange` x) freshRanges) ingredients
       part1 = length freshIngredients
+      edges = nub $ concatMap (\(a, b) -> [a, b]) freshRanges
+      disjointRanges = nub $ sort $ concatMap (splitRange edges) freshRanges
+      part2 = sum $ map (\(a, b) -> b - a) disjointRanges
   print part1
+  print $ length freshRanges
+  -- print edges
+  -- print disjointRanges
+  print part2
 
 -- print freshRanges
 -- print ingredients
