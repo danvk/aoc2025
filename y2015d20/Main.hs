463cb8e6b070b89d237c83ab4a44c05b14bec9e1
Dan Vanderkam <danvdk@gmail.com>
2025-12-05 09:33:52 -0500

    this is clearer with a comprehension

--- y2015d20/Main.hs
+++ y2015d20/Main.hs
@@ -13,7 +13,7 @@ presents n = 10 * sum (map (\x -> n `div` x + x) (filter (\x -> n `mod` x == 0) 
     overcount = if sqrtn * sqrtn == n then 10 * sqrtn else 0
 
 presents2 :: Int -> Int
-presents2 n = 11 * sum (map (\x -> n `div` x) (filter (\x -> n `mod` x == 0) [1 .. 50]))
+presents2 n = 11 * sum [n `div` x | x <- [1 .. 50], n `mod` x == 0]
 
 main :: IO ()
 main = do
