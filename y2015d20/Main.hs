3e3c125ea74ed88039ec5efe76efccd9d7a362c5
Dan Vanderkam <danvdk@gmail.com>
2025-12-04 22:15:53 -0500

    playing around in python

--- y2015d20/Main.hs
+++ y2015d20/Main.hs
@@ -12,10 +12,18 @@ presents n = 10 * sum (map (\x -> n `div` x + x) (filter (\x -> n `mod` x == 0) 
     sqrtn = intSqrt n
     overcount = if sqrtn * sqrtn == n then 10 * sqrtn else 0
 
+presents2 :: Int -> Int
+presents2 n = 11 * sum (map (\x -> (if n `div` x <= 50 then n `div` x else 0) + x) (filter (\x -> n `mod` x == 0) [1 .. sqrtn]))
+  where
+    sqrtn = intSqrt n
+
 main :: IO ()
 main = do
   args <- getArgs
   let n = read @Int $ head args
       houses = zip [1 :: Int ..] $ map presents [1 ..]
       match = find (\(_, num) -> num >= n) houses
+      houses2 = zip [1 :: Int ..] $ map presents2 [1 ..]
+      match2 = find (\(_, num) -> num >= n) houses2
   print match
+  print match2
