37df22d0817910c64376205ed4ae3198258d3c77
Dan Vanderkam <danvdk@gmail.com>
2025-12-06 16:13:33 -0500

    y2015d23 star 1

--- y2015d23/Main.hs
+++ y2015d23/Main.hs
@@ -1,6 +1,8 @@
 -- https://adventofcode.com/2015/day/23
+
+import Data.Map qualified as M
 import System.Environment (getArgs)
-import Text.Read
+import Text.Read (readMaybe)
 
 data Instr
   = Hlf Char
@@ -30,10 +32,40 @@ parseLine str = case words (eraseChars "+," str) of
   ["jio", [r], offset] -> Jio r (loudRead offset)
   _ -> error $ "Unable to parse " ++ str
 
+type State = (Int, M.Map Char Int)
+
+exec :: Instr -> State -> State
+exec (Hlf r) (n, regs) = (n + 1, M.adjust (`div` 2) r regs)
+exec (Tpl r) (n, regs) = (n + 1, M.adjust (* 3) r regs)
+exec (Inc r) (n, regs) = (n + 1, M.adjust (+ 1) r regs)
+exec (Jmp offset) (n, regs) = (n + offset, regs)
+exec (Jie r offset) (n, regs) = (if even (regs M.! r) then n + offset else n + 1, regs)
+exec (Jio r offset) (n, regs) = (if (regs M.! r) == 1 then n + offset else n + 1, regs)
+
+(!?) :: (Ord t, Num t) => [a] -> t -> Maybe a
+[] !? _ = Nothing
+(x : xs) !? i
+  | i < 0 = Nothing
+  | i == 0 = Just x
+  | otherwise = xs !? (i - 1)
+
+-- TODO: is there a vector type for fast indexing?
+step :: [Instr] -> State -> Maybe State
+step instrs s@(idx, _) = fmap (`exec` s) (instrs !? idx)
+
+-- TODO: I think this is a fold
+iterateToNone :: (a -> Maybe a) -> a -> [a]
+iterateToNone fn start = go (Just start)
+  where
+    go Nothing = []
+    go (Just x) = x : go (fn x)
+
 main :: IO ()
 main = do
   args <- getArgs
   let inputFile = head args
   content <- readFile inputFile
-  let x = map parseLine $ lines content
-  print x
+  let instrs = map parseLine $ lines content
+      initState = (0, M.fromList [('a', 0), ('b', 0)])
+  print instrs
+  print $ last $ iterateToNone (step instrs) initState
