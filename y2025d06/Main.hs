350c1d6034503dc1fd6213b5357541872f509451
Dan Vanderkam <danvdk@gmail.com>
2025-12-06 08:24:32 -0500

    y2025d06 star 2

--- y2025d06/Main.hs
+++ y2025d06/Main.hs
@@ -1,12 +1,20 @@
 -- https://adventofcode.com/2025/day/6
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
+import Data.Char
 import Data.List
+import Data.List.Split
 import System.Environment (getArgs)
 
 applyOp :: [String] -> Int
 applyOp ("*" : nums) = product $ map (read @Int) nums
 applyOp ("+" : nums) = sum $ map (read @Int) nums
+
+fn :: [String] -> [String]
+fn (op : nums) = trim op : map reverse (transpose nums)
+
+trim :: String -> String
+trim = dropWhile isSpace . reverse . dropWhile isSpace . reverse
 
 main :: IO ()
 main = do
@@ -14,4 +22,6 @@ main = do
   let inputFile = head args
   content <- readFile inputFile
   let part1 = sum $ map (applyOp . reverse) (transpose $ map words $ lines content)
+      part2 = sum $ map applyOp $ map (fn . reverse . transpose) $ wordsBy (all (== ' ')) $ transpose $ lines content
   print part1
+  print part2
