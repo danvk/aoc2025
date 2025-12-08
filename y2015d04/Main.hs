8fee42ab452cf4e2d2fda4b7312c98da0ea4a3fa
Dan Vanderkam <danvdk@gmail.com>
2025-12-08 12:53:58 -0500

    generating hashes

--- y2015d04/Main.hs
+++ y2015d04/Main.hs
@@ -5,19 +5,19 @@ import Data.ByteString.Base16
 import Data.ByteString.Char8 qualified as B
 import System.Environment (getArgs)
 
-salted_md5 :: String -> Int -> B.ByteString
-salted_md5 salt x = encode $ hash $ B.pack $ salt ++ show x
+saltedMd5 :: String -> Int -> B.ByteString
+saltedMd5 salt x = encode $ hash $ B.pack $ salt ++ show x
 
 isWinner :: String -> Int -> Bool
 isWinner salt x =
-  let h = B.unpack (salted_md5 salt x)
+  let h = B.unpack (saltedMd5 salt x)
    in case h of
         '0' : '0' : '0' : '0' : '0' : _ -> True
         _ -> False
 
 isWinner2 :: String -> Int -> Bool
 isWinner2 salt x =
-  let h = B.unpack (salted_md5 salt x)
+  let h = B.unpack (saltedMd5 salt x)
    in case h of
         '0' : '0' : '0' : '0' : '0' : '0' : _ -> True
         _ -> False
--- y2016d14/Main.hs
+++ y2016d14/Main.hs
@@ -1,15 +1,16 @@
 -- https://adventofcode.com/2016/day/14
+
+import Crypto.Hash.MD5 (hash)
+import Data.ByteString.Base16
+import Data.ByteString.Char8 qualified as B
 import System.Environment (getArgs)
 
-parseLine :: String -> [String]
-parseLine str = case words str of
-  word:rest -> word:rest
-  _ -> error $ "Unable to parse " ++ str
+saltedMd5 :: String -> Int -> B.ByteString
+saltedMd5 salt x = encode $ hash $ B.pack $ salt ++ show x
 
 main :: IO ()
 main = do
   args <- getArgs
-  let inputFile = head args
-  content <- readFile inputFile
-  let x = map parseLine $ lines content
-  print x
+  let salt = head args
+  let hashes = map (saltedMd5 salt) [0 ..]
+  print $ hashes !! 18
