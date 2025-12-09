4e13046d8358e960b9bb43ff3a1f5864e6ba0c45
Dan Vanderkam <danvdk@gmail.com>
2025-12-09 16:15:07 -0500

    attempt to include target in state

--- y2016d19/Main.hs
+++ y2016d19/Main.hs
@@ -8,6 +8,9 @@ import System.Environment (getArgs)
 -- whose turn, elf -> next elf, elf -> num presents
 type State = (Int, M.Map Int Int, M.Map Int Int)
 
+-- attacker, prev(target), elf -> next elf, elf -> num presents
+type State2 = (Int, Int, M.Map Int Int, M.Map Int Int)
+
 step :: State -> State
 step (idx, nexts, presents) = (nextNext, nextNexts, nextPresents)
   where
@@ -17,19 +20,22 @@ step (idx, nexts, presents) = (nextNext, nextNexts, nextPresents)
     numStolen = presents M.! next
     nextPresents = M.adjust (numStolen +) idx (M.delete next presents)
 
-step2 :: State -> State
-step2 (idx, nexts, presents) = trace ("target: " ++ show next ++ " (n=" ++ show numElves ++ ")") (nextElf, nextNexts, nextPresents)
+step2 :: State2 -> State2
+step2 (attacker, preTarget, nexts, presents) =
+  trace
+    ("target: " ++ show target ++ " (n=" ++ show numElves ++ ")")
+    (nextAttacker, nextPreTarget, nextNexts, nextPresents)
   where
     numElves = length nexts
-    numAdvance = numElves `div` 2
-    chain = iterate (nexts M.!) idx
-    preNext = chain !! (numAdvance - 1)
-    next = nexts M.! preNext
-    nextNext = nexts M.! next
-    nextNexts = M.insert preNext nextNext (M.delete next nexts)
-    numStolen = presents M.! next
-    nextPresents = M.adjust (numStolen +) idx (M.delete next presents)
-    nextElf = nextNexts M.! idx
+    target = nexts M.! preTarget
+    postTarget = nexts M.! target
+    nextNexts = M.insert preTarget postTarget (M.delete target nexts)
+    numStolen = presents M.! target
+    nextPresents = M.adjust (numStolen +) attacker (M.delete target presents)
+    nextAttacker = nextNexts M.! attacker
+    preTarget1 = nextNexts M.! preTarget
+    preTarget2 = nextNexts M.! preTarget1
+    nextPreTarget = if even numElves then preTarget1 else preTarget2
 
 main :: IO ()
 main = do
@@ -37,7 +43,7 @@ main = do
   let numElves = (read @Int) (head args)
       nextElf = M.fromList $ zip (numElves : [1 .. (numElves - 1)]) [1 .. numElves]
       presents = M.fromList $ map (,1 :: Int) [1 .. numElves]
-      state = (1, nextElf, presents)
+      state = (1, numElves `div` 2, nextElf, presents)
       states = iterate step2 state
-      part1 = find (\(_, n, _) -> length n == 1) states
+      part1 = find (\(_, _, n, _) -> length n == 1) states
   print part1
