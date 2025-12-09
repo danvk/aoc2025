441350cc2a1d9649c7b0d1294076126316a7d68b
Dan Vanderkam <danvdk@gmail.com>
2025-12-09 09:30:46 -0500

    stroke path

--- y2025d09/Main.hs
+++ y2025d09/Main.hs
@@ -2,9 +2,9 @@
 
 import Data.List
 import Data.List.Split
+import Data.Map.Strict qualified as M
+import Grid
 import System.Environment (getArgs)
-
-type Point = (Int, Int)
 
 parseLine :: String -> Point
 parseLine str = case splitOn "," str of
@@ -14,6 +14,16 @@ parseLine str = case splitOn "," str of
 area :: Point -> Point -> Int
 area (x1, y1) (x2, y2) = (x2 - x1 + 1) * (y2 - y1 + 1)
 
+strokePath :: Grid -> [Point] -> Grid
+strokePath initG pts = g'
+  where
+    pairs = zip pts $ tail pts ++ [head pts]
+    stroke ((x1, y1), (x2, y2)) g
+      | x1 == x2 = M.union g (M.fromList [((x1, y), 'X') | y <- [min y1 y2 + 1 .. max y1 y2 - 1]])
+      | y1 == y2 = M.union g (M.fromList [((x, y1), 'X') | x <- [min x1 x2 + 1 .. max x1 x2 - 1]])
+      | otherwise = error "Invalid pair"
+    g' = foldr stroke initG pairs
+
 main :: IO ()
 main = do
   args <- getArgs
@@ -21,11 +31,19 @@ main = do
   content <- readFile inputFile
   let pts = map parseLine $ lines content
       part1 = maximum $ [area p1 p2 | p1 <- pts, p2 <- pts, p1 < p2]
+      (xs, ys) = unzip pts
+      dims = (1 + maximum xs, 1 + maximum ys)
+      g = M.fromList [(pt, '#') | pt <- pts]
+      g' = strokePath g pts
   print part1
+  putStrLn $ gridToStr dims g
+  putStrLn ""
+  putStrLn $ gridToStr dims g'
 
 -- any more efficient way to go from [(a,b)] -> ([a], [b])?
 -- xs = map fst pts
 -- ys = map snd pts
+-- (xs, ys) = unzip pts  -- <-- yes!
 -- print $ length pts
 -- print (minimum xs, maximum xs)
 -- print (minimum ys, maximum ys)
