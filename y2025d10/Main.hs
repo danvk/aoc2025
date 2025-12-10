57e3423625d88a5a39019662ddee81cae5b9cd23
Dan Vanderkam <danvdk@gmail.com>
2025-12-10 12:52:14 -0500

    create a system of equations

--- y2025d10/Main.hs
+++ y2025d10/Main.hs
@@ -2,20 +2,16 @@
 
 import AocLib
 import Data.Bits
-import Data.Heap qualified
 import Data.List.Split
 import Data.Map.Strict qualified as M
-import Data.Maybe
-import Data.Set qualified as S
 import System.Environment (getArgs)
-import System.IO
 
 data Machine = Machine
   { target :: Int,
     targetStr :: String,
     buttons :: [Int],
     buttonsMap :: [M.Map Int Int],
-    joltages :: M.Map Int Int
+    joltages :: [Int]
   }
   deriving (Show, Eq)
 
@@ -37,45 +33,21 @@ parseLine str = case words (eraseChars "[](){}" str) of
             targetStr = target,
             buttons = map (foldr (\b acc -> acc + 2 ^ b) 0) buttons,
             buttonsMap = map (M.fromList . map (,1)) buttons,
-            joltages = M.fromList $ zip [0 ..] (map (loudRead @Int) $ splitOn "," $ last rest)
+            joltages = map (loudRead @Int) $ splitOn "," $ last rest
           }
   _ -> error $ "Unable to parse " ++ str
 
 -- pressButton :: Int -> Int -> Int
 -- pressButton a b = a `xor` b
 
-bfs :: (Ord a) => (a -> [a]) -> (a -> Int) -> (a -> Bool) -> [a] -> Maybe a
-bfs stepFn weight done starts = go initHeap S.empty
+-- bit mask of unknown variables, list of joltage = bitvector
+machineToEq :: Machine -> (Int, [(Int, Int)])
+machineToEq m = (2 ^ length (buttons m) - 1, eqs)
   where
-    initList = zip (map weight starts) starts
-    initHeap = Data.Heap.fromList initList `asTypeOf` (undefined :: Data.Heap.MinPrioHeap Int a)
-    go h visited = case Data.Heap.view h of
-      Just ((_, val), rest) ->
-        if val `S.member` visited
-          then go rest visited
-          else
-            if done val then Just val else go (insertAll rest $ map (\x -> (weight x, x)) (stepFn val)) (S.insert val visited)
-      Nothing -> Nothing
-    insertAll = foldr Data.Heap.insert
-
-step :: Machine -> Int -> [Int]
-step m x = map (xor x) (buttons m)
-
-stepD :: (a -> [a]) -> ((Int, a) -> [(Int, a)])
-stepD stepFn = fn
-  where
-    fn (n, pt) = map (n + 1,) $ stepFn pt
-
-solveMachine :: Machine -> Maybe (Int, Int)
-solveMachine m = bfs (stepD (step m)) fst (\(_, s) -> s == target m) [(0, 0)]
-
-type State2 = M.Map Int Int
-
-step2 :: Machine -> State2 -> [State2]
-step2 m s = map (M.unionWith (+) s) (buttonsMap m)
-
-solveMachine2 :: Machine -> Maybe (Int, State2)
-solveMachine2 m = bfs (stepD (step2 m)) fst (\(_, s) -> s == joltages m) [(0, M.empty)]
+    eqs = [(bitmaskForIndex i, target) | (i, target) <- zip [0 ..] (joltages m)]
+    btns = buttons m
+    pairs = zip [0 :: Int ..] btns
+    bitmaskForIndex i = sum $ [if b .&. (1 `shiftL` i) > 0 then 2 ^ j else 0 | (j, b) <- pairs]
 
 main :: IO ()
 main = do
@@ -83,7 +55,9 @@ main = do
   let inputFile = head args
   content <- readFile inputFile
   let machines = map parseLine $ lines content
-  print $ map (\m -> (length $ targetStr m, length $ buttons m)) machines
+  -- print $ map (\m -> (length $ targetStr m, length $ buttons m)) machines
+  print machines
+  print $ map machineToEq machines
 
 -- part1 = sum $ map ((fst . fromJust) . solveMachine) machines
 -- print part1
