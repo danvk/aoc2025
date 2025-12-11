3a8c838399928b37ad1005a4c60087bcec478b6e
Dan Vanderkam <danvdk@gmail.com>
2025-12-11 11:36:26 -0500

    cleanup

--- y2025d11/Main.hs
+++ y2025d11/Main.hs
@@ -1,10 +1,22 @@
 -- https://adventofcode.com/2025/day/11
 
 import AocLib
-import Data.List
 import Data.Map.Strict qualified as M
 import Data.Set qualified as S
 import System.Environment (getArgs)
+
+toposort :: (Ord a) => (a -> [a]) -> [a] -> [a]
+toposort neighborFn nodes = fst $ foldr foldFn ([], S.empty) nodes
+  where
+    dfs [] visited = ([], visited)
+    dfs (x : xs) visited =
+      if x `S.member` visited
+        then
+          dfs xs visited
+        else
+          let (nstack, nvisited) = dfs (neighborFn x ++ xs) (S.insert x visited)
+           in (x : nstack, nvisited)
+    foldFn n (stack, visited) = let (nstack, nvisited) = dfs [n] visited in (nstack ++ stack, nvisited)
 
 parseLine :: String -> (String, [String])
 parseLine str = case words (eraseChars ":" str) of
@@ -21,19 +33,6 @@ getCounts g nodes start = go nodes (M.fromList [(start, 1)])
         outs = M.findWithDefault [] n g
         nextCounts = M.fromList $ map (,meCount) outs
 
-toposort :: (Ord a) => (a -> [a]) -> [a] -> [a]
-toposort neighborFn nodes = fst $ foldr foldFn ([], S.empty) nodes
-  where
-    dfs [] visited = ([], visited)
-    dfs (x : xs) visited =
-      if x `S.member` visited
-        then
-          dfs xs visited
-        else
-          let (nstack, nvisited) = dfs (neighborFn x ++ xs) (S.insert x visited)
-           in (x : nstack, nvisited)
-    foldFn n (stack, visited) = let (nstack, nvisited) = dfs [n] visited in (nstack ++ stack, nvisited)
-
 main :: IO ()
 main = do
   args <- getArgs
@@ -43,21 +42,9 @@ main = do
   content2 <- readFile sortedNodesFile
   let edges = map parseLine $ lines content
       sortedNodes = lines content2
-      -- nodes = nub $ concatMap (uncurry (:)) edges
       g = M.fromList edges
       part1 = getCounts g sortedNodes "you" M.! "out"
   print part1
-
-  -- putStrLn $ intercalate "\n" $ toposort (\n -> M.findWithDefault [] n g) nodes
-
-  -- descendents = M.fromList $ map (\k -> (k, floodFill (\n -> M.findWithDefault [] n g) [k])) nodes
-  -- sortedNodes = sortBy (nodeOrder descendents) nodes
-
-  -- putStrLn $ intercalate "\n" sortedNodes
-
-  -- print sortedNodes
-
-  -- part1 = getCounts edges "you" M.! "out"
 
   let svrCounts = getCounts g sortedNodes "svr"
       fftCounts = getCounts g sortedNodes "fft"
@@ -69,11 +56,4 @@ main = do
       dacOut = M.findWithDefault 0 "out" dacCounts
       fftOut = M.findWithDefault 0 "out" fftCounts
       part2 = svrFft * fftDac * dacOut + svrDac * dacFft * fftOut
-  -- print svrCounts
-  print svrFft
-  print svrDac
-  print fftDac
-  print dacFft
-  print dacOut
-  print fftOut
   print part2
