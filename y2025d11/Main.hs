b2eae9901b48d9ffe1c9f4fba3a2d44178625f92
Dan Vanderkam <danvdk@gmail.com>
2025-12-11 10:11:06 -0500

    find distances

--- y2025d11/Main.hs
+++ y2025d11/Main.hs
@@ -3,6 +3,7 @@
 import AocLib
 import Data.Heap qualified
 import Data.Map.Strict qualified as M
+import Data.Set qualified as S
 import System.Environment (getArgs)
 
 parseLine :: String -> (String, [String])
@@ -10,29 +11,56 @@ parseLine str = case words (eraseChars ":" str) of
   input : outputs -> (input, outputs)
   _ -> error $ "Unable to parse " ++ str
 
-bfs :: (a -> [a]) -> (a -> Int) -> (a -> Bool) -> [a] -> [a]
-bfs stepFn weight done starts = go initHeap
+getCounts :: [(String, [String])] -> String -> M.Map String Int
+getCounts edges start = go edgesFromStart (M.fromList [(start, 1)])
   where
-    initList = zip (map weight starts) starts
+    edgesFromStart = dropWhile (\(n, _) -> n /= start) edges
+    go [] m = m
+    go ((n, outs) : xs) m = go xs (M.unionWith (+) m nextCounts)
+      where
+        meCount = M.findWithDefault 0 n m
+        nextCounts = M.fromList $ map (,meCount) outs
+
+getDistances :: (Ord a) => (a -> [a]) -> a -> [(Int, a)]
+getDistances stepFn start = go initHeap S.empty
+  where
+    initList = [(0, start)]
     initHeap = Data.Heap.fromList initList `asTypeOf` (undefined :: Data.Heap.MinPrioHeap Int a)
-    go h = case Data.Heap.view h of
-      Just ((_, val), rest) ->
-        let next = go (insertAll rest $ map (\x -> (weight x, x)) (stepFn val))
-         in if done val then val : next else next
+    go h visited = case Data.Heap.view h of
+      Just (n@(d, val), rest) ->
+        if val `S.member` visited
+          then go rest visited
+          else
+            n : go (insertAll rest $ map (1 + d,) (stepFn val)) (S.insert val visited)
       Nothing -> []
     insertAll = foldr Data.Heap.insert
-
-step :: M.Map String [String] -> [String] -> [[String]]
-step m path = map (: path) $ M.findWithDefault [] (head path) m
-
-part1 :: M.Map String [String] -> String -> String -> Int
-part1 g start stop = length $ bfs (step g) length (\x -> head x == stop) [[start]]
 
 main :: IO ()
 main = do
   args <- getArgs
   let inputFile = head args
   content <- readFile inputFile
-  let g = M.fromList $ map parseLine $ lines content
-  print $ part1 g "you" "out"
-  print $ part1 g "svr" "fft"
+  let edges = map parseLine $ lines content
+      g = M.fromList edges
+  print $ getDistances (\n -> M.findWithDefault [] n g) "svr"
+
+-- part1 = getCounts edges "you" M.! "out"
+-- print part1
+-- let svrCounts = getCounts edges "svr"
+--     fftCounts = getCounts edges "fft"
+--     dacCounts = getCounts edges "dac"
+--     svrFft = M.findWithDefault 0 "fft" svrCounts
+--     svrDac = M.findWithDefault 0 "dac" svrCounts
+--     fftDac = M.findWithDefault 0 "dac" fftCounts
+--     dacFft = M.findWithDefault 0 "fft" dacCounts
+--     dacOut = M.findWithDefault 0 "out" dacCounts
+--     fftOut = M.findWithDefault 0 "out" fftCounts
+--     part2 = svrFft * fftDac * dacOut + svrDac * dacFft * fftOut
+-- -- print svrCounts
+-- print svrFft
+-- print svrDac
+-- print fftDac
+-- print dacFft
+-- print dacOut
+-- print fftOut
+-- print part2
