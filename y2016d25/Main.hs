6bdbc5b67e64ebc1b788d0a4fee885082ddfc66e
Dan Vanderkam <danvdk@gmail.com>
2025-12-11 17:17:06 -0500

    setup for last two days of 2016

---
+++ y2016d25/Main.hs
@@ -0,0 +1,15 @@
+-- https://adventofcode.com/2016/day/25
+import System.Environment (getArgs)
+
+parseLine :: String -> [String]
+parseLine str = case words str of
+  word:rest -> word:rest
+  _ -> error $ "Unable to parse " ++ str
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let inputFile = head args
+  content <- readFile inputFile
+  let x = map parseLine $ lines content
+  print x
