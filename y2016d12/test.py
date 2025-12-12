165ffb323aabee4d70ae6d6b2b10d0aeb161e93e
Dan Vanderkam <danvdk@gmail.com>
2025-12-12 09:45:49 -0500

    test loop program

---
+++ y2016d12/test.py
@@ -0,0 +1,15 @@
+def run(b):
+    a = 0
+    loop = True
+    while loop:
+        c = 2
+        while c != 0:
+            if b == 0:
+                return (a, c)
+            b -= 1
+            c -= 1
+        a += 1
+
+
+for b in range(0, 10):
+    print(f"{b=} (a,c)={run(b)}")
